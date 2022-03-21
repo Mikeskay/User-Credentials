065494b995f41e5476c85082df416b4a47a8c411
Mikeskay <47972946+Mikeskay@users.noreply.github.com>
2022-03-21 23:09:49 +0000

    Create UserCredentialsTest.java

---
+++ test/UserCredentialsTest.java
@@ -0,0 +1,13 @@
+package usercredentials.test;
+
+import usercredentials.base.display.UserCredentialsDisplay;
+
+public class UserCredentialsTest {
+    public static void main(String[] args) {
+
+        UserCredentialsDisplay test = new UserCredentialsDisplay();
+  
+  
+    }
+
+}
